93a1262f9624bb19d4fe0373f2ba0e06409e0180
MoonyDewDrops <julia.brouwervanoudshoorn@gmail.com>
2024-02-15 14:43:31 +0100

    Comments
    Also deleted rest of database & moved responsiveness info to the bottom so its not like "Yo why is this here @ the top?"

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,10 @@
-
-
-
-async function getProductData(file) {
-  const response = await fetch(file);
-  const data = await response.json();
-  setRadarChart(data);
-}
-
-
-getProductData('./assets/data/productData.php');
-
-
-function setProduct(data) {
-  const names = data.map((object) => object.product);
-  const types = data.map((object) => object.type);
-  const prices = data.map((object) => object.price);
-  const url = data.map((object) => object.Image);
-}
-
+//This creates a variable for the slider, and the value it changes. 
 var slider = document.getElementById("myRange");
 var output = document.getElementById("prijs");
+//What the text that displays the price displays. This sets it to the slider value 
 output.innerHTML = slider.value;
 
+//when it changes, the value (so also the text) changes
 slider.oninput = function() {
   output.innerHTML = this.value;
 }
